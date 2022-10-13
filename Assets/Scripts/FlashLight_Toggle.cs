bb89595860ebab9a27d7f7e4de62956138e2d678
Jamie McCready <2459354m@student.gla.ac.uk>
2022-10-13 15:30:23 +0100

    added flashlight with working toggle feature

---
+++ Assets/Scripts/FlashLight_Toggle.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class FlashLight_Toggle : MonoBehaviour
+{
+    public GameObject Light1;
+    public GameObject Light2;
+    public GameObject Light3;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        XRGrabInteractable grabbable = GetComponent<XRGrabInteractable>();
+        grabbable.activated.AddListener(ToggleLight);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ToggleLight(ActivateEventArgs arg)
+    {
+        List<GameObject> Lights = new List<GameObject>();
+        Lights.Add(Light1);
+        Lights.Add(Light2);
+        Lights.Add(Light3);
+        Console.WriteLine(Lights);
+        if(Light1.activeInHierarchy){
+            foreach (GameObject i in Lights){
+                i.SetActive(false);
+                };
+        }else{
+            foreach (GameObject i in Lights){
+                i.SetActive(true);
+                };
+        };
+
+
+
+    }
+}   
+
