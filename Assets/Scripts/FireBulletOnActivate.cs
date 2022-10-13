1e9d3e02030222816f6eb21b3eb31a1734de468f
Jamie Mccready <2459354m@student.gla.ac.uk>
2022-10-13 12:47:08 +0100

    Added some sound effects and curtains

--- Assets/Scripts/FireBulletOnActivate.cs
+++ Assets/Scripts/FireBulletOnActivate.cs
@@ -2,15 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+
 public class FireBulletOnActivate : MonoBehaviour
 {
 
     public GameObject bullet;
     public Transform spawnPoint;
     public float fireSpeed = 20;
+    AudioSource audioData;
     // Start is called before the first frame update
     void Start()
     {
+        audioData = GetComponent<AudioSource>();
         XRGrabInteractable grabbable = GetComponent<XRGrabInteractable>();
         grabbable.activated.AddListener(FireBullet);
 
@@ -26,6 +29,7 @@ public class FireBulletOnActivate : MonoBehaviour
         GameObject spawnedBullet = Instantiate(bullet);
         spawnedBullet.transform.position = spawnPoint.position;
         spawnedBullet.GetComponent<Rigidbody>().velocity  = spawnPoint.forward * fireSpeed;
+        audioData.Play(0);
         Destroy(spawnedBullet, 5);
 
     }
