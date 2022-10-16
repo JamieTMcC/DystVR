0f9e3b52b9bdf7e0f23f642bcdd440612d9237f9
Jamie McCready <2459354m@student.gla.ac.uk>
2022-10-16 16:55:43 +0100

    made it so you can swap between burst fire and single shot mode on the pistol

--- Assets/Scripts/BurstFireOnActivate.cs
+++ Assets/Scripts/BurstFireOnActivate.cs
@@ -10,28 +10,51 @@ public class BurstFireOnActivate : MonoBehaviour
     public float fireSpeed = 5;
     public float spread = 60;
     AudioSource audioData;
+    public AudioClip clip1;
+    public AudioClip clip2;
+    public bool burstmode = false;
+    public XRGrabInteractable grabbable;
     // Start is called before the first frame update
 
 
     void Start()
     {
         audioData = GetComponent<AudioSource>();
-        XRGrabInteractable grabbable = GetComponent<XRGrabInteractable>();
-        grabbable.activated.AddListener(burst);
+        grabbable = GetComponent<XRGrabInteractable>();
+        grabbable.activated.AddListener(fire);
     }
 
-    public void burst(ActivateEventArgs arg){
+
+    public void fire(ActivateEventArgs arg){
+        if(burstmode){
+            burst();
+        }else{
+            FireBullet();
+        }
+    }
+    void Update(){
+        Debug.Log(("grabbable is..."));
+        Debug.Log(grabbable.isSelected);
+        Debug.Log(Input.GetButtonDown("XRI_Left_PrimaryButton") || Input.GetButtonDown("XRI_Right_PrimaryButton"));
+        if(grabbable.isSelected && (Input.GetButtonDown("XRI_Left_PrimaryButton") || Input.GetButtonDown("XRI_Right_PrimaryButton"))){
+            burstmode = !burstmode;
+            audioData.PlayOneShot(clip2);
+        }
+
+    }
+
+    public void burst(){
         float[] randints = {0.0f,0.0f,0.0f};
         for(int i = 0; i < 3; i++){
             for(int j = 0; j < 3; j++){
             randints[j] = Random.Range(0,spread);
             }
-            FireBullet(randints);
+            BurstFireBullet(randints);
         }
         
     }
 
-    public void FireBullet(float[] rotations){
+    public void BurstFireBullet(float[] rotations){
         Vector3 rotationToAdd = new Vector3(rotations[0]/360, rotations[1]/360, rotations[2]/360);
 
         GameObject spawnedBullet = Instantiate(bullet);
@@ -40,6 +63,16 @@ public class BurstFireOnActivate : MonoBehaviour
         spawnedBullet.transform.eulerAngles  = spawnPoint.transform.eulerAngles;
         spawnedBullet.transform.Rotate(rotationToAdd);
         spawnedBullet.GetComponent<Rigidbody>().velocity  = (spawnPoint.forward+rotationToAdd) * fireSpeed;
+        audioData.PlayOneShot(clip1);
+        Destroy(spawnedBullet, 5);
+
+    }
+
+    public void FireBullet(){
+        GameObject spawnedBullet = Instantiate(bullet);
+        spawnedBullet.transform.position = spawnPoint.position;
+        spawnedBullet.GetComponent<Rigidbody>().velocity  = spawnPoint.forward * fireSpeed;
+        Debug.Log(spawnPoint.forward);
         audioData.Play(0);
         Destroy(spawnedBullet, 5);
 
