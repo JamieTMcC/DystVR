40f827ede3a1e120101c1ff7b75790ad66c66e81
Jamie Mccready <2459354m@student.gla.ac.uk>
2022-10-31 19:12:05 +0000

    added terrain in new scene

--- Assets/Scripts/XRGrabInteractable2AttachTransform.cs
+++ Assets/Scripts/XRGrabInteractable2AttachTransform.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+
+
 public class XRGrabInteractable2AttachTransform : XRGrabInteractable
 {
     public Transform leftAttachTransform;
